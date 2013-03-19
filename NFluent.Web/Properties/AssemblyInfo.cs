3c472a83cfff36de363922ddac9c471e113437cb
Thomas PIERRAIN <thomas@pierrain.net>
2013-03-19 01:27:47 +0100

    Updates NFluent.Web metadata.

--- NFluent.Web/Properties/AssemblyInfo.cs
+++ NFluent.Web/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NFluent.Web")]
-[assembly: AssemblyDescription("Smooth your web-related .NET TDD experience with NFluent.Web! NFluent.Web is an extension library to NFluent (for web-related tests). NFluent is an ergonomic assertion library which aims to fluent your .NET TDD experience (based on simple Check.That() assertion statements). NFluent aims your tests to be fluent to write (with an happy 'dot' auto completion experience), fluent to read (i.e. as close as possible to plain English expression), but also fluent to troubleshoot, in a less-error-prone way comparing to the classical .NET test frameworks. NFluent is also directly inspired by the awesome Java FEST Fluent assertion/reflection library (http://fest.easytesting.org/)")]
+[assembly: AssemblyDescription("NFluent.Web is an extension library to NFluent for web-related tests. NFluent is an ergonomic assertion library which aims to fluent your .NET TDD experience (based on simple Check.That() assertion statements). NFluent aims your tests to be fluent to write (with an happy 'dot' auto completion experience), fluent to read (i.e. as close as possible to plain English expression), but also fluent to troubleshoot, in a less-error-prone way comparing to the classical .NET test frameworks. NFluent is also directly inspired by the awesome Java FEST Fluent assertion/reflection library (http://fest.easytesting.org/)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Thomas PIERRAIN (thomas@pierrain.net), Marc-Antoine LATOUR and contributors.")]
 [assembly: AssemblyProduct("NFluent.Web")]
